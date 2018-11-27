46085fc3ff10f2944eab121af2464fe8bad21ee4
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 07:53:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,19 +3,6 @@
 
 def key_for_min_value(name_hash)
   if name_hash.length == 0 
-    return nil 
-  else 
-    array = []
-    name_hash.each do |name, value|
-      array << value 
-    end 
-    low_num = array[0]
-    array.each do |value|
-      low_num = value if  value < low_num
-    end 
-     name_hash.each do |key, value|
-      if min_num == value
-        return key
-      end
-    end 
+    return 
+
 end
